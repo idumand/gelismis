1dfa84d27ef98495faa1c93f71c58012230eca2b
idumand <iduman83516@gmail.com>
2026-08-27 19:51:48 +0300

    x

--- test_ws.js
+++
@@ -1,5 +0,0 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://fstream.binance.com/ws/btcusdt@depth50@100ms');
-ws.on('open', () => console.log('connected'));
-ws.on('message', (data) => { console.log(data.toString().substring(0, 100)); ws.close(); });
-ws.on('error', (err) => { console.log('error', err.message); });
